a25e457648b93ae549c7832f9ac8c28eafa77fe1
Your Name <sanreaksmey01@gmail.com>
2026-06-02 12:31:53 +0700

    Kim

--- Library management system/Form1.cs
+++ Library management system/Form1.cs
@@ -31,5 +31,10 @@ namespace Library_management_system
                      
             }
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
